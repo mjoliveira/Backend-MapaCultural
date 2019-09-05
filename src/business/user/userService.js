642d231cefb5b3a6e305c82a97db32a6c1c8402f
mayara.oliveira <mayara.jesus@acad.pucrs.br>
2019-09-05 17:24:32 -0300

    11/@mjoliveira, @joseph.affeldt: remove testes feitos com o usur.json

--- src/business/user/userService.js
+++ src/business/user/userService.js
@@ -1,9 +1,7 @@
 const userRepository = require("../../infrastructure/user/userRepository");
-const userMock = require("../../../testeJson/users.json");
 module.exports = {
     getUsers: function () {
-        //return userRepository.getUsers();
-        return userMock;
+        return userRepository.getUsers();
     },
     saveUser: function (name) {
         return userRepository.saveUser(name);
