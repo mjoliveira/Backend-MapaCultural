2073617db5b154a09f13bfe4a1a65f5919843a70
Vinicius Lima <kiddlima@outlook.com>
2019-09-06 19:24:21 -0300

    Corrigindo swagger e teste unitário

--- src/api/institutions/institutionsRest.js
+++ src/api/institutions/institutionsRest.js
@@ -2,6 +2,66 @@ const express = require('express');
 const router = express.Router();
 const institutionService = require('../../business/institution/institutionService');
 
+
+/**
+ * @swagger
+ *
+ * definitions:
+ *   Institutions:
+ *     type: object
+ *     properties:
+ *       id:
+ *         type: integer
+ *       nome:
+ *         type: string
+ *       endereco:
+ *         type: string
+ *       tempoVisita:
+ *         type: integer
+ *       descricao:
+ *         type: string
+ *       latitude:
+ *         type: double
+ *       longitude:
+ *         type: double
+ *       email:
+ *         type: string
+ *       telefone:
+ *         type: string
+ *       observacoes:
+ *         type: string
+ *   Institutions-Post:
+ *     type: object
+ *     properties:
+ *       id:
+ *         type: integer
+ *       nome:
+ *         type: string
+ *       endereco:
+ *         type: string
+ *       tempoVisita:
+ *         type: integer
+ *       descricao:
+ *         type: string
+ *       latitude:
+ *         type: double
+ *       longitude:
+ *         type: double
+ *       email:
+ *         type: string
+ *       telefone:
+ *         type: string
+ *       observacoes:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   - name: institutions
+ *     description: Tudo sobre Instituições
+ */
+
 /**
  * @swagger
  *
@@ -13,7 +73,7 @@ const institutionService = require('../../business/institution/institutionServic
  *      content:
  *       application/json:
  *        schema:
- *          $ref: '#/definitions/Institution-Post'
+ *          $ref: '#/definitions/Institutions-Post'
  *
  *    responses:
  *     201:
@@ -21,14 +81,14 @@ const institutionService = require('../../business/institution/institutionServic
  *       content:
  *        application/json:
  *          schema:
- *           $ref: '#/definitions/Institution'
+ *           $ref: '#/definitions/Institutions'
  *
  */
-router.post('/', async function(req, res) {
-    const institution = await institutionService.saveInstitution(req.body);
-    res.status(201);
-    res.json(institution);
-  });
+router.post('/', async function (req, res) {
+  const institution = await institutionService.saveInstitution(req.body);
+  res.status(201);
+  res.json(institution);
+});
 
 
 /**
@@ -48,8 +108,8 @@ router.post('/', async function(req, res) {
  * 
  */
 router.get('/', async (req, res) => {
-    const instituicao = await institutionService.getInstitution();
-    res.json(instituicao);
+  const instituicao = await institutionService.getInstitution();
+  res.json(instituicao);
 });
 
 module.exports = router;
